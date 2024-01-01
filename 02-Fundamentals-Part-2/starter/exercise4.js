9127d362e875e31e1644b249177242b7db2ae9ce
Athanase <amakaridi@gmail.com>
2024-01-02 00:00:33 +0400

    20240101_1

--- 02-Fundamentals-Part-2/starter/exercise4.js
+++ 02-Fundamentals-Part-2/starter/exercise4.js
@@ -8,3 +8,22 @@ console.log(years)
 console.log(ages)
 
 
+for (let exercise = 1; exercise <4; exercise++) {
+    console.log(`-------Starting exercise ${exercise}`)
+    for (let rep = 1; rep<6; rep++){
+        console.log(`--Starting repetition ${rep} XXX`)
+    }
+}
+
+let rep =1;
+while(rep<=10) {
+    console.log(`--Starting rep ${rep} XXX`)
+    rep++
+}
+
+let dice = Math.trunc(Math.random()*6)+1
+while (dice !==6 ) {
+    console.log(`You rolled ${dice}`)
+    dice = Math.trunc(Math.random()*6)+1
+}
+console.log(`You rolled ${dice}`)
